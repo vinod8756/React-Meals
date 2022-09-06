a9aa93f8c256de4cb689fb40c0484ee01d39cf36
vinod8756 <71222796+vinod8756@users.noreply.github.com>
2022-09-06 14:20:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useState , useReducer} from 'react'
+import {useState } from 'react'
 import Header from './Components/Layout/Header'
 import Meals from './Components/Meals/Meals'
 import Cart from './Components/Cart/Cart'
